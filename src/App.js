f4afc54b948788d3ed33525388b8b93d1da0d393
akaihito <FCA22050008@edu.fca.ac.jp>
2025-12-01 13:47:11 +0900

    UI関連のコンポーネントを修正

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ModeSelect from './ModeSelect.js';
 import SoloStudy from './SoloStudy.js';
 import RoomStudy from './RoomStudy.js';
 import JoinRoom from './JoinRoom';
+import Sidebar from './Sidebar';
+import Settings from './Settings';
 
 function App() {
   return (
     <BrowserRouter>
+      <Sidebar />
       <Routes>
         <Route path="/" element={<ModeSelect />} />
         <Route path="/solo" element={<SoloStudy />} />
         <Route path="/room/:roomId" element={<RoomStudy />} />
         <Route path="/join" element={<JoinRoom />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </BrowserRouter>
   );
